7f2f179fe4fbd721d3b300bc618ad0c53fcc866a
Onemo Kang <onemokang@outlook.com>
2023-06-18 00:14:08 -0400

    Deposit function added

--- backend/script.js
+++ backend/script.js
@@ -1,6 +1,7 @@
 let userName = document.getElementById('userName');
 let balance = document.getElementById('balance');
 let newBalance;
+let index;
 let userInput = document.getElementById('userInput');
 
 // set the initial values
@@ -10,9 +11,27 @@ function initialLoad() {
 }
 initialLoad();
 
+function deposit() {
+    userInput = 100;
+    index = Math.sign(userInput); // Math.sign() returns 1 or -1, indicating the sign of the number passed as argument
+
+    if (index === 1) {
+        newBalance = +balance.innerText + +userInput;
+        console.log('new balance: ' + newBalance)
+
+        if (newBalance < 0){
+            newBalance = 0;
+        }
+        balance.innerText = newBalance;
+    } else {
+        console.log('user input error: non-positive number was entered.')
+    }
+}
+
+
 function cashout() {
     userInput = 100;
-    let index = Math.sign(userInput); // Math.sign() returns 1 or -1, indicating the sign of the number passed as argument
+    index = Math.sign(userInput); // Math.sign() returns 1 or -1, indicating the sign of the number passed as argument
 
     if (index === 1) {
         newBalance = balance.innerText - userInput;
@@ -23,8 +42,10 @@ function cashout() {
         }
         balance.innerText = newBalance;
     } else {
-        console.log('user input error - non-positive number was entered.')
+        console.log('user input error: non-positive number was entered.')
     }
 }
 
+document.querySelector('div.depositButton').addEventListener('click', deposit)
 document.querySelector('div.cashoutButton').addEventListener('click', cashout)
+
