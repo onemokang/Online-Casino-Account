11015085c2a1ddc3a06658590e93b2ad5f1858dd
Onemo Kang <onemokang@outlook.com>
2023-06-22 03:22:57 +0000

    fix port issue

--- backend/app.js
+++ backend/app.js
@@ -11,13 +11,13 @@ app.use(express.json());
 
 app.use(cors({origin: '*'}));
 
-app.listen('port', () => {
-    console.log(`App is running on port ${port}`)
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 });
 
 app.get('/', () => {
     console.log(`Welcome to Casino de Monte Fortuna!`)
-});
+}); 
 
 // app.get('/account', (req, res) => {
 //     knex('casino')
